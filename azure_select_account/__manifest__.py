fb30118c77ae77733c9c8c16179dd3c5fa5efd0e
fornaro3 <nathanfornaro30@gmail.com>
2026-01-26 11:10:50 -0300

    Add new screenshot

--- azure_select_account/__manifest__.py
+++ azure_select_account/__manifest__.py
@@ -13,5 +13,5 @@
     'data': [],
     'installable': True,
     'application': False,
-    'images': ['static/description/main_screenshot.png'],
+    'images': ['static/description/main_screenshot_.png'],
 }
